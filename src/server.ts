790b49281f29f885fe7adfd9b60e0713eff52193
Joe Burgess <josephburgess@gmail.com>
2023-02-21 17:39:24 +0200

    adding local memory

--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,7 @@ import { Request, Response, NextFunction } from 'express';
 export function createServer(): express.Express {
   const app = express();
   app.use(express.urlencoded({ extended: true }));
+  app.locals.memory = {};
 
   app.put('/set', (req: Request, res: Response) => {
     return res.status(204).end();
